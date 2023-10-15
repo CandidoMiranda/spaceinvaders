61123a130bc2212669bb3fa98875ff49a9d53620
Cândido Miranda <CandidoMiranda@users.noreply.github.com>
2023-10-15 10:29:19 -0300

    Add lasers class and functionality

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
-from typing import Any
 import pygame
+from laser import Laser
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, constraint, speed):
@@ -11,6 +11,8 @@ class Player(pygame.sprite.Sprite):
         self.ready = True
         self.laser_time = 0
         self.laser_cooldown = 600
+
+        self.lasers = pygame.sprite.Group()
 
     def get_input(self):
         keys = pygame.key.get_pressed()
@@ -39,9 +41,10 @@ class Player(pygame.sprite.Sprite):
             self.rect.right = self.max_x_constraint
 
     def shoot_laser(self):
-        print('shoot laser')
+        self.lasers.add(Laser((self.rect.center), -8, self.rect.bottom))
 
     def update(self):
         self.get_input()
         self.constraint()
         self.recharge()
+        self.lasers.update()
